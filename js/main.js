9812b540a35f7d3ee6dbc965f87b1668499cb2ae
Jairo <jairo.antonucci@hotmail.com>
2021-06-08 13:52:24 -0300

    Commit Aula 02

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+/* Aula 01 - Introdução
+
+var nome = "Jairo Antonucci"
+var idade = 29
+var idade2 = 10
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase);
+console.log(frase.replace("Japão","Brasil"));
+alert(frase.replace("Japão","Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+*/
+
+
+/* Aula 02 - Array e Dicionário
+
+*/
+var lista = ["maça","pêra","laranja"];
+lista.push("uva");
+lista.pop();
+console.log(lista[1]);
+alert(lista);
